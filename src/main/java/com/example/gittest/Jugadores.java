f5a28fd45ec69df143815f30d59b7c080288c123
alvarogarcia94 <alvarogb.barcelona94@gmail.com>
2021-10-08 07:49:54 +0200

    Commit number 2

---
+++ src/main/java/com/example/gittest/Jugadores.java
@@ -0,0 +1,15 @@
+package com.example.gittest;
+
+public class Jugadores {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String procedencia;
+    private Integer dorsal;
+    private String posicion;
+    private Double estatura;
+    private Double peso;
+
+
+}
